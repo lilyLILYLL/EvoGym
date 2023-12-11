0040f861395e159212b3a6ad3e9d8fc1b69e2b47
lilyLILYLL <lydatily@gmail.com>
2023-12-11 15:06:25 +1100

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
+/** @type {import('vite').UserConfig} */
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
-    },
+  plugins: [react()],
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  },
 });
