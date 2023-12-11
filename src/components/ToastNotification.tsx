86cf27f1f242cd6c091cf86a273b8b8b6b117540
lilyLILYLL <lydatily@gmail.com>
2023-12-11 14:11:51 +1100

    fix toast nofication

--- src/components/ToastNotification.tsx
+++ src/components/ToastNotification.tsx
@@ -8,11 +8,11 @@ export const ToastNotification = (props: Props) => {
   return (
     <div
       onAnimationEnd={props.setIsShown}
-      className={`w-fit p-6 sm:p-10 py-4  fixed  rounded-2xl bg-[#f1f1f1]  shadow-lg shadow-slate-200 sm:text-lg  text-md z-50 mr-6 ${
+      className={`w-fit p-6 sm:p-10 py-4  fixed  rounded-2xl bg-[#f1f1f1]  shadow-lg shadow-slate-200 sm:text-lg  text-md z-50 mr-6 sm:translate-x-[-50%] sm:left-[50%] ${
         props.isShown ? "notification" : ""
       }`}
     >
-      Your form has been submited. We will contact you as soon as possile! 🎉 🎉
+      Your form has been submited. We will contact you as soon as possile! 🎉
     </div>
   );
 };
